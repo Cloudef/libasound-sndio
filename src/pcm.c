f10bcee47199f60f1e7b819b6f867d57a14ec03b
Jari Vetoniemi <mailroxas@gmail.com>
2018-11-03 06:13:56 +0200

    tidy up

--- src/pcm.c
+++ src/pcm.c
@@ -647,8 +647,7 @@ snd_pcm_drain(snd_pcm_t *pcm)
    if (pcm->started && sio_stop(pcm->hdl)) {
       WARNX1("stopped");
       pcm->started = false;
-      pcm->written = pcm->position = 0;
-      pcm->avail = 0;
+      pcm->avail = pcm->written = pcm->position = 0;
    }
 
    return (!pcm->started ? 0 : -1);
